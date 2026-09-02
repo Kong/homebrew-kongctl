a71797e6a88cc31f8dcf951af69b32e3b52470f4
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2026-09-02 13:18:56 -0500

    homebrew: publish kongctl 1.15.0 (#8)
    Co-authored-by: Rick Spurgeon <10521262+rspurgeon@users.noreply.github.com>

--- Formula/kongctl.rb
+++ Formula/kongctl.rb
@@ -1,8 +1,8 @@
 class Kongctl < Formula
   desc "Developer CLI for Kong"
   homepage "https://github.com/Kong/kongctl"
-  url "https://github.com/Kong/kongctl/archive/refs/tags/v1.14.0.tar.gz"
-  sha256 "7f92ebe97b355f239cc9d2cf11159c001b21d322cd85d0fda36ffcfb207c8a43"
+  url "https://github.com/Kong/kongctl/archive/refs/tags/v1.15.0.tar.gz"
+  sha256 "a86c09baa409fbe220c29a6f52f4fb98d473e4b1da1d4b935ccdc6cd766415a1"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -10,8 +10,8 @@ class Kongctl < Formula
   def install
     ldflags = %W[
       -X main.version=#{version}
-      -X main.commit=b3808868
-      -X main.date=2026-08-31T15:58:41Z
+      -X main.commit=86e156e0
+      -X main.date=2026-09-02T16:47:12.720631851Z
     ]
 
     ENV["CGO_ENABLED"] = "0"
@@ -21,7 +21,7 @@ class Kongctl < Formula
   test do
     output = shell_output("#{bin}/kongctl version --full")
     assert_match version.to_s, output
-    assert_match "b3808868", output
+    assert_match "86e156e0", output
     assert_match "__kongctl_debug", shell_output("#{bin}/kongctl completion bash")
   end
 end
