dddfaf581693021dd3b94080262ea4512f34ab82
Rick Spurgeon <10521262+rspurgeon@users.noreply.github.com>
2026-09-05 22:28:09 +0000

    kongctl: add 1.15.0 bottle.

--- Formula/kongctl.rb
+++ Formula/kongctl.rb
@@ -5,6 +5,14 @@ class Kongctl < Formula
   sha256 "a86c09baa409fbe220c29a6f52f4fb98d473e4b1da1d4b935ccdc6cd766415a1"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/kong/kongctl"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3e18596078db821cf0abf30ee7137e5342f303639a923cf128df645a19c8d1da"
+    sha256 cellar: :any_skip_relocation, sequoia:       "09937ae65d2231741a927c27c8457345aab0148b72f5c53a98859b233c4b961a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63b3a8ca14d79de6ed02f585c3661b22fb29f211be15db24bfcf59ab7ae1e94c"
+  end
+
   depends_on "go" => :build
 
   def install
